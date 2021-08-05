86e94e6758640d33c818909cfdf6c4d41dd90810
Nico <qtsddxy@163.com>
2021-08-05 14:42:43 +0800

    last commit

---
+++ precache-manifest.e01aa8e642820f2b9283bfed60737926.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24062cf850051123bd0d",
+    "url": "css/app.9a4b72db.css"
+  },
+  {
+    "revision": "2f5f2ca5e19762f0724e",
+    "url": "css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "73f295e1e8ad61e99c13faa804de0528",
+    "url": "index.html"
+  },
+  {
+    "revision": "24062cf850051123bd0d",
+    "url": "js/app.e496a3be.js"
+  },
+  {
+    "revision": "2f5f2ca5e19762f0724e",
+    "url": "js/chunk-vendors.0ad60da6.js"
+  },
+  {
+    "revision": "6bb5600bebe854312ca3977fe456dd65",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "e628cb34d88b8e336da79d5e7c6506e9",
+    "url": "qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
